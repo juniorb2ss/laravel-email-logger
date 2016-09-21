13d36418693e03dfa6d53a2dbe639c0c54953d0f
Carlos Eduardo <juniorb2ss@gmail.com>
2016-09-21 17:01:15 -0300

    Create model

--- src/Config/EmailLogger.php
+++ src/Config/EmailLogger.php
@@ -1,27 +1,5 @@
 <?php
 
 return [
-
-	'driver' => 'redis',
-
-	'connections' => [
-		//
-		'redis' => [
-			'host' => '',
-			'port' => ''
-		];
-
-		//
-		'database' => [
-			'connection' => 'mysql', // Your app connection
-			'database' => 'database', // Database to log
-		];
-
-		//
-		'sns' => [
-			'url' => '',
-			'key' => '',
-			'secret' => '',
-		];
-	];
+	//
 ];
